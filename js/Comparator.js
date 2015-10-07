47aaf7018403ae9e8ca7cff0d40c5de06aa7a927
Tomer Ben Kereth <bk.tomer@gmail.com>
2015-10-07 21:07:09 +0300

    changed a little bit the mobile view 3

--- js/Comparator.js
+++ js/Comparator.js
@@ -859,6 +859,14 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     }, false);
 }
 
+$(document).on('click','.navbar-collapse.in',function(e) {
+
+    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+        $(this).collapse('hide');
+    }
+
+});
+
 
 ChangeVisibility=function show() {
     document.getElementById(arguments[0]).style.display='block';
