8e30b13e364feb0e2aa150dfb378cc9addd14e0f
Tomer Ben Kereth <bk.tomer@gmail.com>
2015-10-07 19:55:11 +0300

    trying to make mobile compatible view

--- js/Comparator.js
+++ js/Comparator.js
@@ -853,6 +853,12 @@ $scope.fillPricesArray=function(){
 });
 
 
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    window.addEventListener('load', function() {
+        setTimeout(function() { window.scrollTo(0, 1); }, 1);
+    }, false);
+}
+
 
 ChangeVisibility=function show() {
     document.getElementById(arguments[0]).style.display='block';
