9318d934f6080927ba63bcaa62ddb50f09f96b20
sayag11 <sayag11@gmail.com>
2015-10-13 11:33:53 -0400

    background color changed again

--- js/Comparator.js
+++ js/Comparator.js
@@ -906,7 +906,7 @@ $(document).on('click','.navbar-collapse.in',function(e) {
 ChangeVisibility=function show() {
     document.getElementById(arguments[0]).style.display='block';
     if(arguments[0]=='home'){
-        document.getElementsByTagName("body")[0].style.backgroundColor = "#6990e1";
+        document.getElementsByTagName("body")[0].style.backgroundColor = "#3f5c9a";
     }
     else{
         document.getElementsByTagName("body")[0].style.backgroundColor = "white";
