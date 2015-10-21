8bf06e5b92bcf866658db53a79f934faa64ca678
Tomer Ben Kereth <bk.tomer@gmail.com>
2015-10-21 17:45:14 +0300

    address bar collapse

--- js/Comparator.js
+++ js/Comparator.js
@@ -889,7 +889,7 @@ function hideAddressBar()
     {
         if(document.height < window.outerHeight)
         {
-            document.body.style.height = (window.outerHeight + 50) + 'px';
+            $(document).body.style.height = (window.outerHeight + 50) + 'px';
         }
 
         setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
